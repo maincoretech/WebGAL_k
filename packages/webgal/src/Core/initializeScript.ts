f58c486342dfec0f1319743d62f57599094495ff
shiftz300 <kasetry300@outlook.com>
2024-03-21 03:13:02 +0800

    none

--- packages/webgal/src/Core/initializeScript.ts
+++ packages/webgal/src/Core/initializeScript.ts
@@ -24,8 +24,9 @@ export const isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // 判断是否
 export const initializeScript = (): void => {
   // 打印初始log信息
   logger.info(__INFO.version);
-  logger.info('Github: https://github.com/OpenWebGAL/WebGAL ');
-  logger.info('Made with ❤ by OpenWebGAL');
+  //logger.info('Github: https://github.com/OpenWebGAL/WebGAL ');
+  //logger.info('Made with ❤ by OpenWebGAL');
+  logger.info('This is shiftz mod version, powered by WebGAL');
   // 激活强制缩放
   // 在调整窗口大小时重新计算宽高，设计稿按照 1600*900。
   if (isIOS) {
