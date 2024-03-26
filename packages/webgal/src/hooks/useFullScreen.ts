810d7158688d2fa897dd4743e2f6231237ef2aa8
shiftz300 <kasetry300@outlook.com>
2024-03-26 12:36:21 +0800

    add tauri

--- packages/webgal/src/hooks/useFullScreen.ts
+++ packages/webgal/src/hooks/useFullScreen.ts
@@ -2,9 +2,11 @@ import { setStorage } from '@/Core/controller/storage/storageController';
 import { RootState } from '@/store/store';
 import { fullScreenOption } from '@/store/userDataInterface';
 import { setOptionData } from '@/store/userDataReducer';
+import { getCurrent } from '@tauri-apps/api/window';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { keyboard } from './useHotkey';
+import { FullScreen } from '@icon-park/react';
 
 export function useFullScreen() {
   const userDataState = useSelector((state: RootState) => state.userData);
@@ -17,19 +19,20 @@ export function useFullScreen() {
     switch (fullScreen) {
       case fullScreenOption.on: {
         if (isEnterGame) {
+          //getCurrent().maximize();
           document.documentElement.requestFullscreen();
           if (keyboard) keyboard.lock(['Escape', 'F11']);
-        };
+        }
         break;
       }
       case fullScreenOption.off: {
         if (document.fullscreenElement) {
+          //getCurrent().setFullscreen(false);
           document.exitFullscreen();
           if (keyboard) keyboard.unlock();
-        };
+        }
         break;
       }
     }
   }, [fullScreen]);
-
-};
+}
