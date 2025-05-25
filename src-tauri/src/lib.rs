0440faa709fb3e6749ff165e1849ffa542e4e86f
shiftz300 <kasetry300@outlook.com>
2025-05-25 14:13:43 +0800

    fix plugin on mobile

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,7 +2,11 @@
 pub fn run() {
   tauri::Builder::default()
     .plugin(tauri_plugin_fs::init())
-    .plugin(tauri_plugin_window_state::Builder::default().build())
+    .setup(|app| {
+      #[cfg(desktop)]
+      app.handle().plugin(tauri_plugin_window_state::Builder::default().build());
+      Ok(())
+    })
     .plugin(tauri_plugin_persisted_scope::init())
     .plugin(tauri_plugin_process::init())
     .run(tauri::generate_context!())
